7f9560f2864c777ac64140a68266036c1cad0c19
Atr07 <adithyaatr@gmail.com>
2025-01-08 23:38:15 +0530

    commit for weather_app - service added

---
+++ weather_app/lib/Services/service_calls.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../Model/weather_model.dart';
+
+class WeatherServices {
+  fetchWeather() async {
+    final response = await http.get(
+      Uri.parse(
+          "https://api.openweathermap.org/"),
+    );
+
+    try {
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        return WeatherData.fromJson(json);
+      } else {
+        throw Exception('Failed to load Weather data');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
