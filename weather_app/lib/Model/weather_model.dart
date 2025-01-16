a1f3851ece7b9aadc692b4e146b44f822bbea5ae
Atr07 <adithyaatr@gmail.com>
2025-01-16 23:13:49 +0530

    commit for weather_app - dash ui #1

--- weather_app/lib/Model/weather_model.dart
+++ weather_app/lib/Model/weather_model.dart
@@ -1,129 +1,25 @@
 class WeatherData {
   final String name;
-  final Temperature temperature;
-
+  final double temperature;
   final int humidity;
-  final Wind wind;
-  final double maxTemperature;
-  final double minTemperature;
-  final int pressure;
-  final int seaLevel;
-  final List<WeatherInfo> weather;
+  final double wind;
+  final String condition;
 
   WeatherData({
     required this.name,
     required this.temperature,
     required this.humidity,
     required this.wind,
-    required this.maxTemperature,
-    required this.minTemperature,
-    required this.pressure,
-    required this.seaLevel,
-    required this.weather,
+    required this.condition,
   });
 
   factory WeatherData.fromJson(Map<String, dynamic> json) {
     return WeatherData(
-      name: json['name'],
-      temperature: Temperature.fromJson(json['main']['temp']),
-      humidity: json['main']['humidity'],
-      wind: Wind.fromJson(json['wind']),
-      maxTemperature: (json['main']['temp_max'] - 273.15), // Kelvin to Celsius
-      minTemperature: (json['main']['temp_min'] - 273.15), // Kelvin to Celsius
-      pressure: json['main']['pressure'],
-      seaLevel: json['main']['sea_level'] ?? 0,
-      weather: List<WeatherInfo>.from(
-        json['weather'].map(
-              (weather) => WeatherInfo.fromJson(weather),
-        ),
-      ),
+      name: json['location']['name'],
+      temperature: json['current']['temp_c'],
+      humidity: json['current']['humidity'],
+      wind: json['current']['wind_kph'],
+      condition: (json['current']['condition']['text']),
     );
   }
 }
-
-class WeatherInfo {
-  final String main;
-
-  WeatherInfo({
-    required this.main,
-  });
-
-  factory WeatherInfo.fromJson(Map<String, dynamic> json) {
-    return WeatherInfo(
-      main: json['main'],
-    );
-  }
-}
-
-class Temperature {
-  final double current;
-
-  Temperature({required this.current});
-
-  factory Temperature.fromJson(dynamic json) {
-    return Temperature(
-      current: (json - 273.15), // Kelvin to Celsius
-    );
-  }
-}
-
-class Wind {
-  final double speed;
-
-  Wind({required this.speed});
-
-  factory Wind.fromJson(Map<String, dynamic> json) {
-    return Wind(speed: json['speed']);
-  }
-}
-
-//Sample Response
-
-/*
-{
-"location": {
-"name": "Kismet",
-"region": "New York",
-"country": "United States of America",
-"lat": 40.6342,
-"lon": -73.2039,
-"tz_id": "America/New_York",
-"localtime_epoch": 1736965249,
-"localtime": "2025-01-15 13:20"
-},
-"current": {
-"last_updated_epoch": 1736964900,
-"last_updated": "2025-01-15 13:15",
-"temp_c": 0.4,
-"temp_f": 32.7,
-"is_day": 1,
-"condition": {
-"text": "Partly cloudy",
-"icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
-"code": 1003
-},
-"wind_mph": 17.4,
-"wind_kph": 28.1,
-"wind_degree": 309,
-"wind_dir": "NW",
-"pressure_mb": 1019.0,
-"pressure_in": 30.1,
-"precip_mm": 0.0,
-"precip_in": 0.0,
-"humidity": 36,
-"cloud": 25,
-"feelslike_c": -5.8,
-"feelslike_f": 21.7,
-"windchill_c": -5.7,
-"windchill_f": 21.7,
-"heatindex_c": 0.3,
-"heatindex_f": 32.6,
-"dewpoint_c": -7.6,
-"dewpoint_f": 18.3,
-"vis_km": 16.0,
-"vis_miles": 9.0,
-"uv": 1.4,
-"gust_mph": 20.1,
-"gust_kph": 32.3
-}
-}*/
--- weather_app/lib/Screens/home.dart
+++ weather_app/lib/Screens/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:geolocator/geolocator.dart';
 
+import '../Model/weather_model.dart';
 import '../Services/service_calls.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,15 +16,23 @@ class HomePage extends StatefulWidget {
 class _WeatherHomeState extends State<HomePage> {
 
   final permissionLocation = Permission.location;
+  WeatherData? weatherInfo;
+  bool? isLoading;
 
   void locationPermissionStatus() async {
     // Request location permission
     final status = await permissionLocation.request();
     if (status == PermissionStatus.granted) {
+      isLoading = true;
       // Get the current location
       final position = await Geolocator.getCurrentPosition();
       print('Latitude: ${position.latitude}, Longitude: ${position.longitude}');
-      WeatherServices().fetchWeather('${position.latitude}', '${position.longitude}');
+      WeatherServices().fetchWeather('${position.latitude}', '${position.longitude}').then((value) {
+        setState(() {
+          weatherInfo = value;
+          isLoading = false;
+        });
+      });
     } else {
       // Permission denied
       print('Location permission denied.');
@@ -32,6 +41,9 @@ class _WeatherHomeState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    bool? mLoading = isLoading;
+    WeatherData? mWeatherInfo = weatherInfo;
 
     return Scaffold(
       appBar: AppBar(
@@ -49,10 +61,120 @@ class _WeatherHomeState extends State<HomePage> {
               onPressed: locationPermissionStatus,
               child: Text('Get Location'),
             ),
+            Center(
+              child: (mLoading != null && !mLoading && mWeatherInfo !=null)
+                  ? WeatherDetail(weather: mWeatherInfo)
+                  : const CircularProgressIndicator(color: Colors.white),
+            ),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
+}
+
+class WeatherDetail extends StatelessWidget {
+  final WeatherData weather;
+
+  const WeatherDetail({
+    super.key,
+    required this.weather
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // for current address name
+        Text(
+          weather.name,
+          style: const TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // for current temperature of my location
+        Text(
+          "${weather.temperature}°C",
+          style: const TextStyle(
+            fontSize: 40,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 30),
+        // for more weather detail
+        Container(
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.wind_power,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(height: 5),
+                        weatherInfoCard(
+                            title: "Wind", value: "${weather.wind}km/h"),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.water_drop,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(height: 5),
+                        weatherInfoCard(
+                            title: "Humidity", value: "${weather.humidity}%"),
+                      ],
+                    ),
+                  ],
+                ),
+                const Divider(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column weatherInfoCard({required String title, required String value}) {
+    return Column(
+      children: [
+        Text(
+          value,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 18,
+          ),
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+        )
+      ],
+    );
+  }
 }
